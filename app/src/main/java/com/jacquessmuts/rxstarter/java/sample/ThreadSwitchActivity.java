7f3766c2e6790e53c8138542df749f90f56a35fe
Jacques Chester Smuts <chester@jacquessmuts.com>
2018-10-22 19:09:14 +0200

    Tally clicks to show explanatino on ThreadSwitchActivity

--- app/src/main/java/com/jacquessmuts/rxstarter/java/sample/ThreadSwitchActivity.java
+++ app/src/main/java/com/jacquessmuts/rxstarter/java/sample/ThreadSwitchActivity.java
@@ -1,6 +1,7 @@
 package com.jacquessmuts.rxstarter.java.sample;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -8,6 +9,7 @@ import com.jacquessmuts.rxstarter.R;
 import com.jacquessmuts.rxstarter.java.BaseActivity;
 import com.jakewharton.rxbinding2.view.RxView;
 
+import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
@@ -28,29 +30,45 @@ public class ThreadSwitchActivity extends BaseActivity {
         Button buttonGoodThreading = findViewById(R.id.buttonGoodThreading);
 
         TextView textView = findViewById(R.id.textView);
+        TextView textViewExplanation = findViewById(R.id.textViewExplanation);
 
-
-
+        //this is the same as just setting the onClick normally, inside a try{}catch{]
         rxSubs.add(RxView.clicks(buttonNoThreading)
                 .subscribe( random -> {
                     //set tally to textview
                     textView.setText(String.valueOf(getRandomNumber()));
                 }, Timber::e));
 
-
+        //This is the good code. It does threading well
         rxSubs.add(RxView.clicks(buttonGoodThreading)
-                .observeOn(Schedulers.computation()) //following functions will be on computation thread
+                .observeOn(Schedulers.computation()) //all following functions will be on computation thread
                 .map(input -> getRandomNumber())
-                .observeOn(AndroidSchedulers.mainThread()) //following functions will be on ui thread
+                .observeOn(AndroidSchedulers.mainThread()) //all following functions will be on ui thread
                 .subscribe( random -> {
                     textView.setText(String.valueOf(random));
                 }, Timber::e));
 
+        //merge both buttons's emissions and tally the total number of clicks between them both
+        rxSubs.add(Observable.merge(RxView.clicks(buttonNoThreading),
+                    RxView.clicks(buttonGoodThreading))
+                .map(input -> 1).scan((total, nuValue) -> total + nuValue) //keep a running tally.
+                .subscribe( tally -> {
+                    if (tally > 3){
+                        textViewExplanation.setVisibility(View.VISIBLE);
+                    }
+                }, Timber::e));
+
+
     }
 
+    /**
+     * Returns a random number from 1-1000, after a 1500 miillisecond wait.
+     * Simulates a long-running calculation
+     * @return a random int from 0-1000
+     */
     private int getRandomNumber(){
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
